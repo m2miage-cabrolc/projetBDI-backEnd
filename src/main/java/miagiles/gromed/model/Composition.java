38124df5586d277e0c1ea61c8e21d9d387117679
l3miage-konushbb <knbegimay@gmail.com>
2023-01-26 21:02:12 +0100

    modif composition

--- src/main/java/miagiles/gromed/model/Composition.java
+++ src/main/java/miagiles/gromed/model/Composition.java
@@ -18,12 +18,12 @@ public class Composition {
 
     @Id
     @ManyToOne
-    @JoinColumn(name="medicament")
+    @JoinTable(name="medicament")
     private Medicament medicament;
 
     @Id
     @ManyToOne
-    @JoinColumn(name="substance")
+    @JoinTable(name="substance")
     private Substance substance;
 
     public String getDosage() {
@@ -42,19 +42,12 @@ public class Composition {
         this.substance.setCodeSubstance(substance);
     }
 
-    public void setMedicament(Medicament medicament) {
-        this.medicament = medicament;
-    }
-
-    public void setMedicament(long medicament){
-        this.medicament.setCodeCIS(medicament);
-    }
-
     public String getReference() {
         return reference;
     }
 
-    public void setReference(String reference) {
-        this.reference = reference;
+    public Medicament getMedicament() {
+        return medicament;
     }
+
 }
