9e696bce2f4bbc48c49e846583ac769b8b6ab90c
l3miage-konushbb <knbegimay@gmail.com>
2023-01-30 16:58:41 +0100

    feat recherche meds

--- src/main/java/miagiles/gromed/repository/PresentationRepository.java
+++ src/main/java/miagiles/gromed/repository/PresentationRepository.java
@@ -1,9 +1,18 @@
 package miagiles.gromed.repository;
 
+import miagiles.gromed.entity.Medicament;
 import miagiles.gromed.entity.Presentation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface PresentationRepository extends CrudRepository<Presentation, Long> {
+
+
+
+    @Query("SELECT p.medicaments from pres p join p.medicaments m where m.denomination LIKE %:denom%")
+    Iterable<Medicament> findMedsByDenom (@Param("denom") String denom);
 }
