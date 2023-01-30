c1722027f36da94aad6d2d9ebe0a802678f4f8cd
l3miage-konushbb <knbegimay@gmail.com>
2023-01-30 17:14:58 +0100

    feat all meds

--- src/main/java/miagiles/gromed/controller/MedicamentController.java
+++ src/main/java/miagiles/gromed/controller/MedicamentController.java
@@ -1,12 +1,23 @@
 package miagiles.gromed.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import miagiles.gromed.entity.Medicament;
 import miagiles.gromed.repository.MedicamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/api/meds")
 public class MedicamentController {
     @Autowired
     private MedicamentRepository repository;
+
+    @GetMapping(value="/")
+    public ResponseEntity<Iterable<Medicament>> getAll(HttpServletResponse httpServletResponse){
+        return ResponseEntity.ok(repository.findAll());
+    }
 
 }
